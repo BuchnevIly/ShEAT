f7d4d45ff328d721af529c5535fccd3cf3961117
Andrey Katcuba <andreuka2009@mail.ru>
2017-05-12 23:15:56 +0400

    Статистика DTO

--- ShEAT/Models/DTO/StatisticResponse.cs
+++ ShEAT/Models/DTO/StatisticResponse.cs
@@ -7,5 +7,8 @@ namespace ShEAT.Models.DTO
 {
     public class StatisticResponse
     {
+        public List<DateTime> FullStatistic { get; set; }
+
+        public List<double> FullListCalories { get; set; }
     }
 }
